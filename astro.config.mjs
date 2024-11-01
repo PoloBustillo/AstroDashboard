5199abd79d2a63d58b64f32c54a73a2962a74ce1
PoloBustillo <39663418+PoloBustillo@users.noreply.github.com>
2024-11-01 17:10:58 -0600

    add icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import netlify from '@astrojs/netlify';
+import netlify from "@astrojs/netlify";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: 'server',
-  adapter: netlify()
+  integrations: [
+    tailwind(),
+    icon({
+      iconDir: "src/icons",
+    }),
+  ],
+  output: "server",
+  adapter: netlify(),
 });
