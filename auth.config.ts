5bb76b32bc6828822f5483a11792d67dd2c1b3bf
PoloBustillo <39663418+PoloBustillo@users.noreply.github.com>
2024-11-03 01:16:32 -0600

    add GitHub provider to auth configuration

--- auth.config.ts
+++ auth.config.ts
@@ -1,5 +1,6 @@
 import Google from "@auth/core/providers/google";
 import Discord from "@auth/core/providers/discord";
+import GitHub from "@auth/core/providers/github";
 import { defineConfig } from "auth-astro";
 
 export default defineConfig({
@@ -11,6 +12,10 @@ export default defineConfig({
     Google({
       clientId: import.meta.env.GOOGLE_CLIENT_ID,
       clientSecret: import.meta.env.GOOGLE_CLIENT_SECRET,
+    }),
+    GitHub({
+      clientId: import.meta.env.GITHUB_CLIENT_ID,
+      clientSecret: import.meta.env.GITHUB_CLIENT_SECRET,
     }),
   ],
   callbacks: {
