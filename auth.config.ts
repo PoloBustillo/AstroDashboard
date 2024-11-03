96578382e14c193b60dbd3caae53420ecc05e98f
PoloBustillo <39663418+PoloBustillo@users.noreply.github.com>
2024-11-03 14:39:51 -0600

    add Twitter provider to auth configuration and implement sign-in button functionality

--- auth.config.ts
+++ auth.config.ts
@@ -1,10 +1,15 @@
 import Google from "@auth/core/providers/google";
 import Discord from "@auth/core/providers/discord";
 import GitHub from "@auth/core/providers/github";
+import X from "@auth/core/providers/twitter";
 import { defineConfig } from "auth-astro";
 
 export default defineConfig({
   providers: [
+    X({
+      clientId: import.meta.env.X_CLIENT_ID,
+      clientSecret: import.meta.env.X_CLIENT_SECRET,
+    }),
     Discord({
       clientId: import.meta.env.DISCORD_CLIENT_ID,
       clientSecret: import.meta.env.DISCORD_CLIENT_SECRET,
