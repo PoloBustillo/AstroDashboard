2f8d0c9f0350f283c3cf5ef0be3ee03804dbca3c
PoloBustillo <39663418+PoloBustillo@users.noreply.github.com>
2024-11-04 20:47:14 -0600

    add new SVG icons for notifications, machine learning, users, and dashboard; update middleware for session handling and redirect logic; refactor ProfileAvatar component and index page layout

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -3,14 +3,18 @@ import { getSession } from "auth-astro/server";
 
 export const onRequest = defineMiddleware(async (context, next) => {
   let session = await getSession(context.request);
-  console.log("MIDDLEWARE", session);
+  console.log("MIDDLEWARE", next, context);
   if (context.url.pathname.includes("sign-in") && session) {
-    return context.redirect("/");
+    let redirectUrl = context.url.searchParams.get("next") || "/";
+    return context.redirect(redirectUrl);
   }
 
   if (context.url.pathname.includes("sign-up") && session) {
     return context.redirect("/");
   }
 
+  if (context.url.pathname.includes("protected") && session == null) {
+    return context.redirect("/auth/sign-in?next=/protected/recursos");
+  }
   return next();
 });
