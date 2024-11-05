3d1f8c1103282d2ee110574e0bc235e10baeea0b
PoloBustillo <leopoldobeguiluz1@hotmail.com>
2024-11-04 21:37:18 -0600

    add Netlify Blobs integration for user API; update GET handler to set nail blobs for construction and beauty stores

--- src/pages/api/users/[id].ts
+++ src/pages/api/users/[id].ts
@@ -1,12 +1,14 @@
 import type { APIRoute } from "astro";
+import { getStore } from "@netlify/blobs";
+import type { Context } from "@netlify/functions";
 
-const usernames = ["Sarah", "Chris", "Yan", "Elian"];
-export const GET: APIRoute = ({ params, request }) => {
-  const id = params.email as string;
+export const GET: APIRoute = async ({ params, request }) => {
+  const construction = await getStore({
+    name: "my-store",
+    siteID: "a909c73d-1dce-4ce5-9aa4-b08b5ad02a2c",
+    token: "nfp_mjDFQ4Sorg33ddU5VNcDJ6nwiFKG9hMN8d4c",
+  });
+  await construction.set("nails", "For general carpentry");
 
-  return new Response(
-    JSON.stringify({
-      name: "ID",
-    }),
-  );
+  return new Response("Nail blobs set for Construction and Beauty stores");
 };
